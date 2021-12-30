016dc445c1446e9d214e9bd0e9e5e1effd92f121
angelDariaux <angeldariaux@gmail.com>
2021-12-29 22:14:03 -0600

    terminada la clase de inner y outer HTML

--- manipulacionDelDom/indexManipulacionDelDOM.js
+++ manipulacionDelDom/indexManipulacionDelDOM.js
@@ -1,7 +1,7 @@
 'use strict';
 window.addEventListener('load', () => { 
 
-const container = document.querySelector('p.parrafo1');
+const container = document.querySelector('p.parrafo1');//seleciona un parrafo con la clase parrafo1
 const h3 = document.createElement('h3');
 const texto = document.createTextNode('Texto del h3 creado con JS');
 
@@ -14,10 +14,24 @@ container.append('hola 2', document.createElement('div'));// hola 2 es un texto 
 
           // document.insertBefore(node, referencia);
 
-          // node.outerHTMO (leer) & node.innerHTML (escribir)
+          // node.outerHTMl (leer) & node.innerHTML (escribir)
 
+let h2 = document.querySelector('h2.seccion-subTitulo');
 
+console.group(`node.outerHTML (leer) & node.innerHTML (escribir)`);
+  console.log(`usando el outerHTML:`,h2.outerHTML );
+  console.log(`usando el innerHTML:`,h2.innerHTML );
+  
+                //node.outerHTML.repalce
+  
+let parrafoSeccion = document.querySelector('p.parrafo3');
+let nuevoParrafo = parrafoSeccion.outerHTML.replace('ESPANOL', 'INGLES') //replaza la palabra espanol por ingles
+parrafoSeccion.innerHTML = nuevoParrafo;//aqui agregamos el cambio al DOM
 
+console.log(`usando .outerHTML.repalce:`,nuevoParrafo);
+  
+console.groupEnd();
+  
 
 
 
